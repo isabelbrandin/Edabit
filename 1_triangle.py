1528feb145b633384c6b6cee914dec8202eb7ad9
Einar Persson <me@einarpersson.com>
2020-01-23 11:13:31 +0100

    Fix comments

--- 1_triangle.py
+++ 1_triangle.py
@@ -1,6 +1,6 @@
 def tri_area(base, height):
     return base * height / 2
 
-# test
 
+# Tester
 print(tri_area(10, 6))
--- 3_coordinates.py
+++ 3_coordinates.py
@@ -3,7 +3,9 @@ def convert_cartesian(x_list, y_list):
     result = []
     for i in range(length):
         result.append([x_list[i], y_list[i]])
-    
+
     return result
 
+
+# Tester
 print(convert_cartesian([9, 8, 3], [1, 1, 1]))
